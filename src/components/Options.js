3b42795d94226e5515569254c1b0590aa20d2264
zymikcs <zymik.official@gmail.com>
2023-09-09 09:47:08 +0300

    React-Quiz App

---
+++ src/components/Options.js
@@ -0,0 +1,28 @@
+export default function Options({ question, dispatch, answer }) {
+    const hasAnswered = answer !== null;
+
+    return (
+        <div className="options">
+            {question.options.map((option, index) => (
+                <button
+                    className={`btn btn-option ${
+                        index === answer ? 'answer' : ''
+                    } ${
+                        hasAnswered
+                            ? index === question.correctOption
+                                ? 'correct'
+                                : 'wrong'
+                            : ''
+                    }`}
+                    key={option}
+                    disabled={answer !== null}
+                    onClick={() =>
+                        dispatch({ type: 'newAnswer', payload: index })
+                    }
+                >
+                    {option}
+                </button>
+            ))}
+        </div>
+    );
+}
